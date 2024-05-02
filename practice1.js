cc244b29eb6180eb2e0cb71a2ff8f54f5a11aa1a
Noman <nomanmia128@gmail.com>
2024-05-02 20:23:27 +0400

    added javascipt

---
+++ practice1.js
@@ -0,0 +1,29 @@
+// prictice problem 1
+var money = 1000;
+var apple = 400;
+var orange = 300;
+var total = apple + orange;
+var muchMoney = money - total;
+//console.log(muchMoney)
+
+// prictice problem 2
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physices = 35.45;
+var stucent = 99.50;
+var total = mathematics + biology + chemistry + physices;
+//console.log(total)
+
+//prictice probem 3
+var firstLine = 'I am going to be';
+var secondLine = 'an awesome web developer';
+var oneLine = firstLine + secondLine;
+var oneLine = firstLine + ' ' + secondLine;
+console.log(oneLine)
+
+// prictice problem 4
+var number = 119;
+var divison = 5;
+var reminder = number % divison;
+console.log(reminder)
